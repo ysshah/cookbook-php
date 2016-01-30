c1b65759d88362d1ad1313ee0d5750b23d585869
Yash Shah <yashshah0127@gmail.com>
2016-01-30 02:59:11 -0800

    Testing postgres connection

---
+++ web/test.php
@@ -0,0 +1,21 @@
+<?php
+# This function reads your DATABASE_URL config var and returns a connection
+# string suitable for pg_connect. Put this in your app.
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+# Here we establish the connection. Yes, that's all.
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
+# Now let's use the connection for something silly just to prove it works:
+$result = pg_query($pg_conn, "SELECT * FROM ingredients");
+
+if (!pg_num_rows($result)) {
+    echo "Your connection is working, but your database is empty.";
+} else {
+    echo "Rows in your database:\n";
+    while ($row = pg_fetch_row($result)) {
+        echo $row;
+    }
+}
+?>
