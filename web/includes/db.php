36eed3a26b1b679a210e3be26e2a05eec8a75701
Yash Shah <yashshah0127@gmail.com>
2016-08-18 23:11:33 +0000

    Add cookie options in db.php

--- web/includes/db.php
+++ web/includes/db.php
@@ -1,4 +1,7 @@
 <?php
 extract(parse_url($_ENV["DATABASE_URL"]));
 pg_connect("user=$user password=$pass host=$host dbname=".substr($path, 1));
+
+session_set_cookie_params(1209600);
+session_start();
 ?>
